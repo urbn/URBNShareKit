ca69cda2782dc813dd6aed699de047106d70b197
Ryan Garchinsky <ryang1428@gmail.com>
2015-01-15 10:29:56 -0500

    Added URBNActivityViewController

---
+++ Example/Pods/Target Support Files/Pods-Tests-URBNShareKit/Pods-Tests-URBNShareKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tests_URBNShareKit : NSObject
+@end
+@implementation PodsDummy_Pods_Tests_URBNShareKit
+@end
---
+++ Pod/Classes/URBNActivityViewController.h
@@ -0,0 +1,13 @@
+//
+//  URBNActivityViewController.h
+//  Pods
+//
+//  Created by Ryan Garchinsky on 1/15/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface URBNActivityViewController : UIActivityViewController
+
+@end
