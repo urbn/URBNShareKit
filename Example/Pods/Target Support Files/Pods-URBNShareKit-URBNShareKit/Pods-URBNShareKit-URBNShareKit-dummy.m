e8779642b943f94e94e2a3794d5f5c07ef652b39
Ryan Garchinsky <ryang1428@gmail.com>
2016-10-20 13:32:55 -0400

    [MOBI-72] Getting ShareKit up to date

--- Example/Pods/Target Support Files/Pods-URBNShareKit-URBNShareKit/Pods-URBNShareKit-URBNShareKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_URBNShareKit_URBNShareKit : NSObject
-@end
-@implementation PodsDummy_Pods_URBNShareKit_URBNShareKit
-@end
---
+++ Example/Pods/Target Support Files/URBNShareKit/URBNShareKit-umbrella.h
@@ -0,0 +1,13 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "URBNActivityViewController.h"
+#import "URBNBodyProvider.h"
+#import "URBNImageProvider.h"
+#import "URBNSafariActivity.h"
+#import "URBNShareKit.h"
+
+FOUNDATION_EXPORT double URBNShareKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char URBNShareKitVersionString[];
+
